71276be4822e7c1edb9d21c8a8b3d0b39bf5061a
Seongwon Han <seongwonhan88@gmail.com>
2019-07-30 22:55:36 +0900

    profile receiver added

--- members/models/profile.py
+++ members/models/profile.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from members.apps import MembersConfig
@@ -11,3 +13,9 @@ class NormalUserProfile(models.Model):
     class Meta:
         db_table = MembersConfig.name + '_normal_user_profile'
 
+
+@receiver(post_save, sender=get_user_model())
+def create_profile(sender, instance, **kwargs):
+    if kwargs.get('created', False):
+        NormalUserProfile.objects.create(user=instance)
+        print('profile created')
