7035f991657860ec1b4d0b60c73d48d4d2ad4dd3
Seongwon Han <seongwonhan88@gmail.com>
2019-07-31 02:17:01 +0900

    static/media root local setup and uwsgi setup

--- backend_api/settings.py
+++ backend_api/settings.py
@@ -141,6 +141,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # REDIS
 REDIS_HOST = host = os.getenv('REDIS_HOST', 'localhost')
