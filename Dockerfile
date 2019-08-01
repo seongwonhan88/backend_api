72e822df5c605211f3d9f8824b4dfa6329c9168d
Seongwon Han <seongwonhan88@gmail.com>
2019-08-02 00:16:00 +0900

    docker-compose added

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,7 @@
 FROM python:3
-
-RUN apt-get update && apt-get -y install \
-    libpq-dev
-
-WORKDIR /app
-ADD    ./requirements.txt   /app/
-RUN    pip install -r requirements.txt
-
-ADD    ./backend_api   /app/backend_api/
-ADD    ./manage.py      /app/
-
-CMD ["python", "manage.py", "runserver", "0:8000"]
+ENV PYTHONUNBUFFERED 1
+RUN mkdir /code
+WORKDIR /code
+COPY requirements.txt /code/
+RUN pip install -r requirements.txt
+COPY . /code/
--- backend_api/settings.py
+++ backend_api/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', None)
+SECRET_KEY = 'pi-(4_vl3e#dj)_n#lvf5j&ut__9xiz6%iaf!rid^*(brbmd3r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -100,9 +100,10 @@ AUTH_USER_MODEL = 'members.User'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('BACKEND_DB_NAME', None),
-        'HOST': os.environ.get('BACKEND_DB_HOST', None),
-        'PORT': os.environ.get('BACKEND_DB_PORT', None)
+        'NAME': os.environ.get('BACKEND_DB_NAME', 'postgres'),
+        'USER': os.environ.get('BACKEND_DB_USER', 'postgres'),
+        'HOST': os.environ.get('BACKEND_DB_HOST', 'db'),
+        'PORT': os.environ.get('BACKEND_DB_PORT', 5432)
     }
 }
 
